9bd1c56681666de78295d7b015631c858a812173
weicheng <weicheng2138@gmail.com>
2021-09-26 23:10:35 +0800

    get rid of useless plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/scrollTopObserver.client.js', mode: 'client' },
-    { src: '~/plugins/scrollDirectionObserver.client.js', mode: 'client' },
-  ],
+  plugins: [{ src: '~/plugins/scrollTopObserver.client.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
